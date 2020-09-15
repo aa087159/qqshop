d4bfdddabbd930eb4986f07641e4f8551d72ebca
Vivian Chang <aa087159@gmail.com>
2020-09-15 14:03:21 +0200

    .

--- index.js
+++ index.js
@@ -50,7 +50,10 @@ app.use('/api/postMessages', (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
+	app.use(express.static(path.join(__dirname, '/client/dist')));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, '/client/dist', 'index.html'));
+	});
 }
 
 const port = process.env.PORT || 8080;

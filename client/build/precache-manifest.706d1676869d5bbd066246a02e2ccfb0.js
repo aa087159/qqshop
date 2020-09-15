b32d4184e29a8cf8b919875e0445cd7994949ca4
Vivian Chang <aa087159@gmail.com>
2020-09-15 14:05:20 +0200

    .

---
+++ client/build/precache-manifest.706d1676869d5bbd066246a02e2ccfb0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde1cf04b5d30638391f78739f2cec56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abc6ff1e4f2d376954b1",
+    "url": "/static/css/2.c0c74b16.chunk.css"
+  },
+  {
+    "revision": "39f71e56d5363d79b0f5",
+    "url": "/static/css/main.6abfab27.chunk.css"
+  },
+  {
+    "revision": "abc6ff1e4f2d376954b1",
+    "url": "/static/js/2.29300515.chunk.js"
+  },
+  {
+    "revision": "39f71e56d5363d79b0f5",
+    "url": "/static/js/main.358ab7bc.chunk.js"
+  },
+  {
+    "revision": "645b28c45bd2079db4f4",
+    "url": "/static/js/runtime-main.bb6b6926.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);

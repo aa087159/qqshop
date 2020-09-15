8559ae6640a0e61174ed00f17c17adbc4676c589
Vivian Chang <aa087159@gmail.com>
2020-09-15 13:52:17 +0200

    .

--- index.js
+++ index.js
@@ -18,19 +18,19 @@ app.use(express.json());
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '/client')));
+	app.use(express.static('client/build'));
 	// app.get('*', (req, res) => {
 	// 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	// });
 }
 
-app.get('/', (req, res) => {
-	client.connect((err) => {
-		res.json({
-			message: 'doggies!',
-		});
-	});
-});
+// app.get('/', (req, res) => {
+// 	client.connect((err) => {
+// 		res.json({
+// 			message: 'doggies!',
+// 		});
+// 	});
+// });
 
 app.post('/api/postMessages', (req, res) => {
 	client.connect((error) => {
